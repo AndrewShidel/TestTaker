c76e4acb2c2cf133302e49972c14ace64415f770
AndrewShidel <andrew@shidel.com>
2014-10-28 23:00:15 -0400

    Implemented all question sub classes

--- src/Questions/Matching.java
+++ src/Questions/Matching.java
@@ -1,32 +1,42 @@
 package Questions;
 
 import java.util.ArrayList;
+import java.util.Dictionary;
 import java.util.Scanner;
 
 public class Matching  extends Question{
-	private ArrayList<String> choices;
-	private ArrayList<Integer> answer;
+	private ArrayList<String> col1Choices, col2Choices;
+	private Dictionary<Integer, Integer> answer;
 	public Matching(boolean needsAnswer){
 		System.out.println("Enter the prompt for your Matching question:");
 		Scanner scanner = new Scanner(System.in);
 		prompt = scanner.nextLine();
 		System.out.println("Enter the number of choices:");
 		int numChoices = scanner.nextInt();
-		for (int i=0; i<numChoices; i++){
-			System.out.println("Enter option #"+i+":");
-			choices.add(scanner.nextLine());
+		
+		for (int i=1; i<=numChoices; i++){
+			System.out.println("Enter column 1 row "+i+":");
+			col1Choices.add(scanner.nextLine());
+		}
+		for (int i=1; i<=numChoices; i++){
+			System.out.println("Enter column 2 row "+i+":");
+			col2Choices.add(scanner.nextLine());
 		}
 		if (needsAnswer){
-			for (int i=0; i<choices.size(); i++){
-				System.out.println("Which option matches option #"+i+"?");
-				answer.add(scanner.nextInt());
+			for (int i=0; i<col1Choices.size(); i++){
+				System.out.println("Which item in column 2 matches item "+i+" from column 1?");
+				answer.put(i, scanner.nextInt());
 			}
 		}
 		scanner.close();
 	}
 	@Override
 	public void display() {
-		// TODO Auto-generated method stub
-		
+		System.out.println(prompt);
+		for (int i=0;)
+		if (answer.size()>0){
+			System.out.println("The Correct answer is: ");
+			
+		}
 	}
 }
